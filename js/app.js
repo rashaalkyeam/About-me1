5e159be0f20a75e3d4e69499aeac549b8345334f
Ghaidaamoh <ghaidaa97mohammad@gmail.com>
2021-04-28 16:11:55 +0300

    this file update by ghaidaa

--- js/app.js
+++ js/app.js
@@ -1,40 +1,64 @@
 'use strict'
-let nationality= prompt('Do you think that I have the Jordanian nationality?\n pleas enter yes or no, y or n').toLowerCase();
-let age= prompt('Do you think that I am less than 25 years old?\n pleas enter yes or no, y or n').toLowerCase();
-let live= prompt('Do you think I live in Amman?\n pleas enter yes or no, y or n').toLowerCase();
-let study= prompt('Do you think I studied computer science?\n pleas enter yes or no, y or n').toLowerCase();
-let car= prompt('Do you think I own a car?\n pleas enter yes or no, y or n').toLowerCase();
+function fromWhere (){
+let nationality = prompt('Do you think that I have the Jordanian nationality?\n pleas enter yes or no, y or n').toLowerCase();
+if (nationality == 'yes' || nationality == 'y') {
+    console.log('What you think is correct');
+    alert('What you think is correct');
+} else {
+    console.log('What you think is not true');
+    alert('What you think is not true');
+}}
 
 
+function myAge (){
+let age = prompt('Do you think that I am less than 25 years old?\n pleas enter yes or no, y or n').toLowerCase();
+if (age == 'yes' || age == 'y') {
+    console.log('What you think is correct');
+    alert('What you think is correct');
+} else {
+    console.log('What you think is not true');
+    alert('What you think is not true');
+}}
 
-if(nationality=='yes'||nationality=='y'){
-    console.log('What you think is correct')
-}else{
+
+function place (){
+let live = prompt('Do you think I live in Amman?\n pleas enter yes or no, y or n').toLowerCase();
+if (live == 'yes' || live == 'y') {
+    console.log('What you think is correct');
+    alert('What you think is correct');
+} else {
+    console.log('What you think is not true');
+    alert('What you think is not true');
+}}
+
+
+function myStudy(){
+let study = prompt('Do you think I studied computer science?\n pleas enter yes or no, y or n').toLowerCase();
+if (study == 'yes' || study == 'y') {
+    console.log('What you think is correct');
+    alert('What you think is correct');
+} else {
+    console.log('What you think is not true');
+    alert('What you think is not true');
+
+}}
+
+ 
+function haveCar(){
+let car = prompt('Do you think I own a car?\n pleas enter yes or no, y or n').toLowerCase();
+
+if (car == 'yes' || car == 'y') {
+    console.log('What you think is correct');
+    alert('What you think is correct');
+} else {
     console.log('What you think is not true')
-    
-}
-if(age=='yes'||age=='y'){
-    console.log('What you think is correct')
-}else{
-    console.log('What you think is not true')
-    
-}
-if(live=='yes'||live=='y'){
-    console.log('What you think is correct')
-}else{
-    console.log('What you think is not true')
-    
-}
-if(study=='yes'||study=='y'){
-    console.log('What you think is correct')
-}else{
-    console.log('What you think is not true')
-    
-}
-if(car=='yes'||car=='y'){
-    console.log('What you think is correct')
-}else{
-    console.log('What you think is not true')
-    
-}
+    alert('What you think is not true')
+
+}}
+fromWhere ();
+myAge ();
+ place ();
+ myStudy();
+ haveCar();
+
 
