e2e7b2bfdbcfc3349fada060112b0462b3ebdb4d
rashaalkyeam <rasha.alqyyam@gmail.com>
2021-05-06 22:25:43 +0300

    add

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 'use strict'
+let score=0;
+
 function fromWhere (){
 let nationality = prompt('Do you think that I have the Jordanian nationality?\n pleas enter yes or no, y or n').toLowerCase();
 if (nationality == 'yes' || nationality == 'y') {
     console.log('What you think is correct');
     alert('What you think is correct');
+    score++;
 } else {
     console.log('What you think is not true');
     alert('What you think is not true');
@@ -15,6 +18,7 @@ let age = prompt('Do you think that I am less than 25 years old?\n pleas enter y
 if (age == 'yes' || age == 'y') {
     console.log('What you think is correct');
     alert('What you think is correct');
+    score++;
 } else {
     console.log('What you think is not true');
     alert('What you think is not true');
@@ -26,6 +30,7 @@ let live = prompt('Do you think I live in Amman?\n pleas enter yes or no, y or n
 if (live == 'no' || live == 'n') {
     console.log('What you think is correct');
     alert('What you think is correct');
+    score++;
 } else {
     console.log('What you think is not true');
     alert('What you think is not true');
@@ -37,6 +42,7 @@ let study = prompt('Do you think I studied computer science?\n pleas enter yes o
 if (study == 'yes' || study == 'y') {
     console.log('What you think is correct');
     alert('What you think is correct');
+    score++;
 } else {
     console.log('What you think is not true');
     alert('What you think is not true');
@@ -50,17 +56,51 @@ let car = prompt('Do you think I own a car?\n pleas enter yes or no, y or n').to
 if (car == 'no' || car == 'n') {
     console.log('What you think is correct');
     alert('What you think is correct');
+    score++;
 } else {
     console.log('What you think is not true')
     alert('What you think is not true')
 
 }}
+
+function guess(){
+    
+    let num=102;
+for(let i=0;i<4;i++){
+    let numguess=parseInt(prompt("Let's play a guessing game,\n guess the number\n  I'll tell you if it's near or far, or even if you answer correctly."));
+    if(numguess>num){
+        alert('very high');
+    } else if(num>numguess){
+        alert('very low');
+    } else if (num==numguess){
+        alert('correct answer');
+        score++;
+        break;
+    }
+}
+}
+function read(){
+    for(let j=0; j<6; j++){
+    let readguess=prompt("Guess what are my favorite fields of reading?\nFor example: romance, action, poetry, history ...").toLowerCase();
+    if(readguess=="romance"||readguess=="poetry"||readguess=="history"){
+
+        alert('correct answer');
+        score++;
+        break;
+    }else{
+        alert('Not correct answer')
+    }
+
+}
+}
+
 fromWhere ();
 myAge ();
  place ();
  myStudy();
  haveCar();
-
- alert(`Thankyou ${userName}`);
+guess();
+read();
+ alert(`Thankyou ${userName} Your score${score} Questions correctly`);
 
 
